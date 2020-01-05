106ce248e031038f0585091aef8b6e925204a10e
Gael UTARD <gael.utard@kisis.fr>
2020-01-05 13:16:58 +0100

    Temperature chart

--- domotic/urls.py
+++ domotic/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from.views import AttributeListView, ChartView
+from.views import AttributeListView, ChartView, TemperatureChartView
 
 urlpatterns = [
     path('', AttributeListView.as_view()),
     path('chart/<int:pk>/', ChartView.as_view(), name='chart'),
+    path('chart/temperature/', TemperatureChartView.as_view(), name='temperature_chart'),
 ]
--- domotic/views.py
+++ domotic/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
-from .models import Attribute
+from .models import Attribute, Device
 
 
 class AttributeListView(ListView):
@@ -14,4 +14,16 @@ class ChartView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['values'] = self.object.value_set.order_by('timestamp')
+        return context
+
+
+class TemperatureChartView(TemplateView):
+    template_name = 'domotic/temperature_chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rooms'] = {
+            device.name: device.attribute_set.get(name="Température").value_set.order_by('timestamp')
+            for device in Device.objects.all()
+        }
         return context
