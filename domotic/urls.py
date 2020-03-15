afa0c080ed752c643d81637ba9d0e8caf4a86cc5
Gael UTARD <gael.utard@kisis.fr>
2020-03-15 09:33:44 +0100

    Shutter move

--- domotic/urls.py
+++ domotic/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from.views import AttributeListView, ChartView, TemperatureChartView
+from.views import AttributeListView, ChartView, TemperatureChartView, ShutterMove
 
 urlpatterns = [
     path('', AttributeListView.as_view()),
     path('chart/<int:pk>/', ChartView.as_view(), name='chart'),
     path('chart/temperature/', TemperatureChartView.as_view(), name='temperature_chart'),
+    path('shutter/toggle/', ShutterMove.as_view(), name='shutter_all_toggle'),
 ]
--- domotic/views.py
+++ domotic/views.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
+from serial import Serial
+from struct import pack
 
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, RedirectView
 from django.utils.timezone import now
 
 from .models import Attribute, Device
@@ -30,3 +32,47 @@ class TemperatureChartView(TemplateView):
             for device in Device.objects.all()
         }
         return context
+
+
+class ShutterMove(RedirectView):
+    DOWN = 0x00
+    UP = 0x01
+    TOGGLE = 0x02
+
+    url = '/'
+
+    def post(self, target=0x01, cmd=TOGGLE, group=True):
+        CODE = 0x0092
+
+        # Paylod
+        self.mode = 0x01 if group else 0x02  # 0=bound 1=group 2=short 3=ieee
+        src = 0x01
+        dst = 0x01
+        payload = pack('!BHBBB', self.mode, target, src, dst, cmd)
+
+        # Length
+        length = len(payload).to_bytes(2, 'big')
+
+        # Checksum
+        code = CODE.to_bytes(2, 'big')
+        checksum = code[0] ^ code[1]
+        checksum ^= length[0] ^ length[1]
+        for byte in self.payload:
+            checksum ^= byte
+        checksum = bytes([checksum])
+
+        # Raw data
+        data = code + length + checksum + payload
+
+        # Encoded data
+        encoded = bytearray([0x01])
+        for byte in data:
+            if byte < 0x10:
+                encoded.extend([0x02, byte ^ 0x10])
+            else:
+                encoded.append(byte)
+        encoded.append(0x03)
+
+        # Send packet
+        with Serial(port='/dev/ttyS0', baudrate=115200, timeout=5) as serial:
+            serial.write(bytes(encoded))
